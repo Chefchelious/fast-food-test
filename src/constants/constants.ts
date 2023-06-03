b0874dda1503db0ac85fa769ed6dc38f9776c50e
Dmitrii Morrows <trollman1993@gmail.com>
2023-06-03 16:39:05 +0600

    удалены дефолтные картинки, и добавлены новые в соответсвии с name

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,13 +1,17 @@
 import {TItem} from "../types/types";
 
-import foodImg from '../img/food.png';
-import drinkImg from '../img/drink.png';
+import hamburger from '../img/hamburger.png';
+import chiburger from '../img/chiburger.png';
+import fries from '../img/fries.png';
+import coffee from '../img/coffee.png';
+import tea from '../img/tea.png'
+import cola from '../img/cola.png'
 
 export const ITEMS: TItem[] = [
-    {name: 'Hamburger', price: 80, image: foodImg},
-    {name: 'Cheeseburger', price: 90, image: foodImg},
-    {name: 'Fries', price: 45, image: foodImg},
-    {name: 'Coffee', price: 70, image: drinkImg},
-    {name: 'Tea', price: 50, image: drinkImg},
-    {name: 'Cola', price: 40, image: drinkImg},
+    {name: 'Hamburger', price: 80, image: hamburger},
+    {name: 'Cheeseburger', price: 90, image: chiburger},
+    {name: 'Fries', price: 45, image: fries},
+    {name: 'Coffee', price: 70, image: coffee},
+    {name: 'Tea', price: 50, image: tea},
+    {name: 'Cola', price: 40, image: cola},
 ];
